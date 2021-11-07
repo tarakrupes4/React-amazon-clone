c88d55a9cc5a047eb1d941c02ac033290cd0f474
Tarak Rupesh <tarakrupesh4@gmail.com>
2021-11-07 16:15:02 +0530

    nav bar commit..

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
-
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 function Header() {
   return (
     <div className="header">
@@ -12,20 +13,25 @@ function Header() {
 
       <div className="header_search">
         <input type="text" className="header_searchInput" />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header_nav">
         <div className="header_option">
-          <span className="header_optionLineOne">hello user</span>
-          <span className="header_optionLineTwo">sign in</span>
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
         </div>
         <div className="header_option">
-          <span className="header_optionLineOne">Return</span>
-          <span className="header_optionLineTwo">orders</span>
+          <span className="header_optionLineOne">Returns</span>
+          <span className="header_optionLineTwo">& Orders</span>
         </div>
         <div className="header_option">
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
+        </div>
+        <div className="header_optionBasket">
+          <ShoppingCartIcon />
+          <span className="header_optionLineTwo header_basketCount">0</span>
         </div>
       </div>
     </div>
