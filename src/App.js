fd8646a002090e183b23bd56c4694dbbc200c3f6
Tarak Rupesh <tarakrupesh4@gmail.com>
2021-11-08 17:20:05 +0530

    Router commit...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
